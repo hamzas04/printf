f97b3a2ca4abe53e1d4cd45c60e27ec6fa114aa4
hamzas04 <samraoui.hamza@gmail.com>
2023-04-25 19:32:20 -0700

    0x11. C - printf

--- _printf.c
+++ _printf.c
@@ -1,6 +1,15 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * _printf - prints formatted output to stdout
+ * @format: the format string
+ *
+ * Return: the number of characters printed
+ *
+ */
+ 
 int _printf(const char *format, ...)
 {
 	va_list args;
@@ -26,6 +35,24 @@ int _printf(const char *format, ...)
 				case 'i':
 					printed_chars += printf("%d", va_arg(args, int));
 					break;
+				case 'b':
+					{
+						unsigned int num = va_arg(args, unsigned int);
+						int binary[32];
+						int i = 0;
+
+						while (num > 0) {
+							binary[i] = num % 2;
+							num = num / 2;
+							i++;
+						}
+
+						for (int j = i - 1; j >= 0; j--) {
+							printf("%d", binary[j]);
+						}
+						printed_chars += i;
+						break;
+					}
 				case '%':
 					putchar('%');
 					++printed_chars;
@@ -47,5 +74,5 @@ int _printf(const char *format, ...)
 
 	va_end(args);
 
-	return printed_chars;
+	return (printed_chars);
 }
