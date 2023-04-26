a71666e0bfa68bcc5f8d81eabbb407e3fdb903fe
hamzas04 <samraoui.hamza@gmail.com>
2023-04-25 17:31:32 -0700

    1.Education is when you read the fine print. Experience is what you get if you dont

--- _printf.c
+++ _printf.c
@@ -1,92 +1,51 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-/**
- * _printf - prints formatted output to stdout
- * @format: the format string
- *
- * Return: the number of characters printed
- *(excluding the null byte used to end output to strings)
- */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count = 0;
+	int printed_chars = 0;
+
 	va_start(args, format);
 
-
-	while (*format != '\0') {
-		if (*format == '%') {
-			format++;
-
-
-			switch (*format) {
-				case 'c': {
-					char c = (char) va_arg(args, int);
-					putchar(c);
-					count++;
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			++format;
+			switch (*format)
+			{
+				case 'c':
+					putchar(va_arg(args, int));
+					++printed_chars;
 					break;
-				}
-				case 's': {
-					const char *s = va_arg(args, const char *);
-					while (*s != '\0') {
-						putchar(*s);
-						s++;
-						count++;
-					}
+				case 's':
+					printed_chars += printf("%s", va_arg(args, char *));
 					break;
-				}
 				case 'd':
-				case 'i': {
-					int num = va_arg(args, int);
-					printf("%d", num);
-					count += snprintf(NULL, 0, "%d", num);
+				case 'i':
+					printed_chars += printf("%d", va_arg(args, int));
 					break;
-				}
-				case 'u': {
-					unsigned int num = va_arg(args, unsigned int);
-					printf("%u", num);
-					count += snprintf(NULL, 0, "%u", num);
-					break;
-				}
-				case 'o': {
-					unsigned int num = va_arg(args, unsigned int);
-					printf("%o", num);
-					count += snprintf(NULL, 0, "%o", num);
-					break;
-				}
-				case 'x':
-				case 'X': {
-					unsigned int num = va_arg(args, unsigned int);
-					printf(*format == 'x' ? "%x" : "%X", num);
-					count += snprintf(NULL, 0, "%x", num);
-					break;
-				}
-				case 'p': {
-					void *ptr = va_arg(args, void *);
-					printf("%p", ptr);
-					count += snprintf(NULL, 0, "%p", ptr);
-					break;
-				}
-				case '%': {
+				case '%':
 					putchar('%');
-					count++;
+					++printed_chars;
 					break;
-				}
 				default:
 					putchar('%');
 					putchar(*format);
-					count += 2;
+					printed_chars += 2;
 					break;
 			}
-		} else {
-			putchar(*format);
-			count++;
 		}
-		format++;
+		else
+		{
+			putchar(*format);
+			++printed_chars;
+		}
+		++format;
 	}
 
 	va_end(args);
 
-	return (count);
+	return printed_chars;
 }
