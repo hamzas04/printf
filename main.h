542ba242c6e1aaa612d1a47431f7c911b7009ed9
hamzas04 <samraoui.hamza@gmail.com>
2023-04-25 17:12:25 -0700

    0. I m not going anywhere. You can print that wherever you want to. I m here and I m a Spur for life

--- main.h
+++ main.h
@@ -2,14 +2,14 @@
 #define MAIN_H
 
 /**
- * struct format_t - Struct to store format specifiers and their associated functions
+ * struct format_t - Struct to store format specifiers and their associated
  * @specifier: the format specifier character
  * @function: a function pointer to the function that handles the specifier
  */
 typedef struct format_t
 {
-    char specifier;
-    int (*function)(char **, va_list);
+	char specifier;
+	int (*function)(char **, va_list);
 } format_t;
 
 /* Custom printf function */
