4712c98c0870698e72e1b3c18aca6e0132de0d88
hamzas04 <samraoui.hamza@gmail.com>
2023-04-25 19:22:03 -0700

    0x11. C - printf

--- _printf.c
+++ _printf.c
@@ -1,23 +1,14 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-/**
- * _printf - prints formatted output to stdout
- * @format: the format string
- *
- * Return: the number of characters printed
- */
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int printed_chars = 0;
-	char buffer[1024] = {0};
-	char *buf_ptr = buffer;
-	int remaining = sizeof(buffer);
 
 	va_start(args, format);
 
-	while (*format && remaining > 1)
+	while (*format)
 	{
 		if (*format == '%')
 		{
@@ -25,100 +16,36 @@ int _printf(const char *format, ...)
 			switch (*format)
 			{
 				case 'c':
-					*buf_ptr++ = va_arg(args, int);
-					--remaining;
+					putchar(va_arg(args, int));
 					++printed_chars;
 					break;
 				case 's':
-					printed_chars += snprintf(buf_ptr, remaining, "%s", va_arg(args, char *));
-					buf_ptr += printed_chars;
-					remaining -= printed_chars;
+					printed_chars += printf("%s", va_arg(args, char *));
 					break;
 				case 'd':
 				case 'i':
-					printed_chars += snprintf(buf_ptr, remaining, "%d", va_arg(args, int));
-					buf_ptr += printed_chars;
-					remaining -= printed_chars;
+					printed_chars += printf("%d", va_arg(args, int));
 					break;
-				case 'u':
-					printed_chars += snprintf(buf_ptr, remaining, "%u", va_arg(args, unsigned int));
-					buf_ptr += printed_chars;
-					remaining -= printed_chars;
-					break;
-				case 'o':
-					printed_chars += snprintf(buf_ptr, remaining, "%o", va_arg(args, unsigned int));
-					buf_ptr += printed_chars;
-					remaining -= printed_chars;
-					break;
-				case 'x':
-					printed_chars += snprintf(buf_ptr, remaining, "%x", va_arg(args, unsigned int));
-					buf_ptr += printed_chars;
-					remaining -= printed_chars;
-					break;
-				case 'X':
-					printed_chars += snprintf(buf_ptr, remaining, "%X", va_arg(args, unsigned int));
-					buf_ptr += printed_chars;
-					remaining -= printed_chars;
-					break;
-				case 'p':
-					printed_chars += snprintf(buf_ptr, remaining, "%p", va_arg(args, void *));
-					buf_ptr += printed_chars;
-					remaining -= printed_chars;
-					break;
-				case 'b':
-					{
-						unsigned int value = va_arg(args, unsigned int);
-						int bit_pos = 0;
-						while (bit_pos < 32 && remaining > 1)
-						{
-							*buf_ptr++ = ((value >> (31 - bit_pos)) & 0x1) + '0';
-							++bit_pos;
-							--remaining;
-							++printed_chars;
-						}
-					}
-					break;
-				case 'S':
-					{
-						char *str = va_arg(args, char *);
-						while (*str && remaining > 1)
-						{
-							if (*str < ' ' || *str >= 127)
-							{
-								printed_chars += snprintf(buf_ptr, remaining, "\\x%02X", (unsigned char)*str);
-								buf_ptr += printed_chars;
-								remaining -= printed_chars;
-							}
-							else
-							{
-								*buf_ptr++ = *str;
-								--remaining;
-								++printed_chars;
-							}
-							++str;
-						}
-					}
+				case '%':
+					putchar('%');
+					++printed_chars;
 					break;
 				default:
-					*buf_ptr++ = '%';
-					*buf_ptr++ = *format;
-					remaining -= 2;
+					putchar('%');
+					putchar(*format);
 					printed_chars += 2;
 					break;
 			}
 		}
 		else
 		{
-			*buf_ptr++ = *format;
-			--remaining;
+			putchar(*format);
 			++printed_chars;
 		}
 		++format;
 	}
 
 	va_end(args);
-
-	fwrite(buffer, 1, buf_ptr - buffer, stdout);
 
 	return printed_chars;
 }
